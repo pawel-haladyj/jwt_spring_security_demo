94701f67625d14923d740ba06c09e46e4ffec4a1
Pawel Haladyj <pawel@haladyj.pl>
2020-03-24 11:27:34 +0100

    initial commit

---
+++ src/main/java/pl/haladyj/jwtspringsecuritydemo/JwtSpringSecurityDemoApplication.java
@@ -0,0 +1,13 @@
+package pl.haladyj.jwtspringsecuritydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtSpringSecurityDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtSpringSecurityDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/haladyj/jwtspringsecuritydemo/JwtSpringSecurityDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.haladyj.jwtspringsecuritydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtSpringSecurityDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
